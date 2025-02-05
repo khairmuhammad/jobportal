41ec41f5b520fece93bdfa0aa6f15354f3938de9
Khair Muhammad <khairmuhammadmemon@healthhub.kr>
2025-02-05 16:52:33 +0500

    #Completed the development for the recruiter profile

--- src/main/java/com/khair/jobportal/controller/UsersController.java
+++ src/main/java/com/khair/jobportal/controller/UsersController.java
@@ -49,7 +49,7 @@ public class UsersController {
             return "register";
         }
         usersService.addNewUser(users);
-        return "dashboard";
+        return "redirect:/dashboard/";
     }
 
     @GetMapping("/login")
---
+++ src/main/java/com/khair/jobportal/services/RecruiterProfileService.java
@@ -0,0 +1,25 @@
+package com.khair.jobportal.services;
+
+import com.khair.jobportal.entity.RecruiterProfile;
+import com.khair.jobportal.repository.RecruiterProfileRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+
+    private final RecruiterProfileRepository recruiterProfileRepository;
+
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+    }
+
+    public Optional<RecruiterProfile> getRecruiterProfileById(int id) {
+        return recruiterProfileRepository.findById(id);
+    }
+
+    public RecruiterProfile addNew(RecruiterProfile recruiterProfile) {
+        return recruiterProfileRepository.save(recruiterProfile);
+    }
+}
